fd2f9c6ba4b00d7614d0bbfa25d32962022fccc9
dnlaylo <laylomarga@gmail.com>
2024-10-28 21:41:08 +0800

    Array, while loop, and input prompt

--- input_numbers.py
+++ input_numbers.py
@@ -1,6 +1,15 @@
 # Array to store all entries
+entries = []
+
 # Loop for continuous input prompt and validation
-# Prompt user to input a number from 1-50
+while True:
+    try:
+        # Prompt user to input a number from 1-50
+        entry = int(input("Please input a number within 1-50: "))
+
+    except:
+        print("")
+       
 # If statement for checking input
 # If valid, proceed to storing in the array
 # If input is invalid, display count of all valid inputs within its number group/range
