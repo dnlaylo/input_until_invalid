58894d60a166e8c1b80d0dff449bf0258440f958
dnlaylo <laylomarga@gmail.com>
2024-10-28 21:33:22 +0800

    Pseudocode

---
+++ input_numbers.py
@@ -0,0 +1,6 @@
+# Array to store all entries
+# Loop for continuous input prompt and validation
+# Prompt user to input a number from 1-50
+# If statement for checking input
+# If valid, proceed to storing in the array
+# If input is invalid, display count of all valid inputs within its number group/range
