0c85322bc604f415fe7a928697d08e9d7783a6ba
dnlaylo <laylomarga@gmail.com>
2024-10-28 21:49:22 +0800

    Valid input definition, storing in array, and error message

--- input_numbers.py
+++ input_numbers.py
@@ -7,9 +7,15 @@ while True:
         # Prompt user to input a number from 1-50
         entry = int(input("Please input a number within 1-50: "))
 
+        # If statement for checking input
+        if entry < 1 or entry > 50:
+            print("Input is not valid.\n")
+            break
+
+        # If valid, proceed to storing in the array
+        entries.append(entry)
+
     except:
-        print("")
-       
-# If statement for checking input
-# If valid, proceed to storing in the array
+        print("Input is not valid. Integers only.\n")
+
 # If input is invalid, display count of all valid inputs within its number group/range
